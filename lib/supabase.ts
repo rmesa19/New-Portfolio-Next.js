9b0d4397a3ca3c83673e7eb96d3c8dcc0ac1580b
Rudy <rmesa19@gmail.com>
2025-09-10 17:14:38 -0400

    updated env variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 // Use the **service role key** only in server environments (middleware, API routes).
