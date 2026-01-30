ab0aa1040efaa4e2a8e8864750ffafcf07f35236
Rudy <rmesa19@gmail.com>
2026-01-29 21:01:27 -0500

    queens facts call added

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,41 @@ import HobbiesSection from "@/components/hobbies-section"
 import { EducationSection } from "@/components/education-section"
 
 export default function HomePage() {
+  const fetchRandomFact = async () => {
+      try {
+        const res = await fetch("/api/facts?random=true&limit=1")
+        const data = await res.json()
+        if (Array.isArray(data) && data.length > 0) {
+          return data[0]
+        } else if (data.fact_text) {
+          return data
+        }
+        return { fact_text: "No fact available right now." }
+      } catch (err) {
+        console.error("Error fetching fact:", err)
+        return { fact_text: "Error fetching fact." }
+      }
+    }
+
+    const handleNewFact = async () => {
+
+    setTimeout(async () => {
+      const fact = await fetchRandomFact();
+
+      await fetch("/api/audit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        event_type: "porfolio visit",
+        path: "/",
+      }),
+      });
+
+    }, 300)
+  }
+
+  const fact = handleNewFact();
+  
   return (
     <div className="min-h-screen bg-background flex">
       <div className="hidden md:block">
